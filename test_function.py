9429c5e918ef3bdbeea9f3aeff6ce0f3c2cdf9b1
hmayesBU <hmayes@bournemouth.ac.uk>
2024-10-24 16:01:14 +0100

    Updated test_function

--- test_function.py
+++ test_function.py
@@ -1,24 +1,29 @@
-"""
-Function to allow demonstration of how to run a function
-"""
-
 def test_function(num1, num2):
     """
-    Lots of metadata in here to tell you all about the function
-    it wil be over many lines
+    Function to demonstrate how a function works, and how it is used.
+
+    Parameters:
+    num1 (int or float): The first number.
+    num2 (int or float): The second number.
+
+    Returns:
+    result: The result of the sum of the inputs
+        - int or float
+
+    Example Usage:
+    --------------
+    test_function(4, 5)  # Output: 9
+    test_function(10.5, 2.5)  # Output: 13
+
     """
 
     print("The first number is: ", num1)
     print("The second number is: ", num2)
 
     result = num1 + num2
-    print(result)
 
     return result
 
 #This is outside of the function - you can call it here>>
-print(test_function(1,2))
-print(test_function(2.6,2.0))
-print(test_function(3,2.6))
-print(test_function(859,-262))
+
             
